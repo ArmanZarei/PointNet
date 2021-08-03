dd5c09637998285415b01f5d02b3039fc85d6306
ArmanZarei <armanzarei1378@gmail.com>
2021-08-03 10:06:09 -0700

    Add utility functions

---
+++ utils.py
@@ -0,0 +1,24 @@
+import plotly.graph_objects as go
+import numpy as np
+
+def plot_pointcloud(pointcloud):
+  fig = go.Figure(data=[go.Scatter3d(
+  x=pointcloud[:, 0],
+  y=pointcloud[:, 1],
+  z=pointcloud[:, 2],
+  mode='markers',
+  marker=dict(
+    size=2,
+    color='#34495e', 
+    colorscale='Viridis',
+    opacity=0.8
+  ))])
+  fig.show()
+
+def read_off(file):
+  if 'OFF' != file.readline().strip():
+    raise('Not a valid OFF header')
+  n_vertices, n_faces, __ = tuple([int(s) for s in file.readline().strip().split(' ')])
+  vertices = np.array([[float(s) for s in file.readline().strip().split(' ')] for i_vert in range(n_vertices)])
+  faces = np.array([[int(s) for s in file.readline().strip().split(' ')][1:] for i_face in range(n_faces)])
+  return vertices, faces
